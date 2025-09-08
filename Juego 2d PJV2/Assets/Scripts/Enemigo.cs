e17c281a9c7ecc5eff674f9421dea36c5a02a9d1
franb21 <franbrg2@gmail.com>
2025-09-08 02:49:09 -0300

    Enemigos con daño y camara
    Daño con colisiones
    Cámara siguiendo al jugador

---
+++ Juego 2d PJV2/Assets/Scripts/Enemigo.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    [Header("Configuracion de enemigo")]
+    [SerializeField] private float vida;
+    [SerializeField] private float damage;
+    [SerializeField] float velocidad = 5f;
+    [SerializeField] Transform jugador;
+    private Rigidbody2D miRigidbody2D;
+    private Vector2 direccion;
+
+    private void Awake()
+    {
+        miRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    //Movimiento al jugador
+    private void FixedUpdate()
+    {
+        direccion = (jugador.position - transform.position).normalized;
+        miRigidbody2D.MovePosition(miRigidbody2D.position + direccion * (velocidad * Time.fixedDeltaTime));
+    }
+
+    //Daño al enemigo
+    public void RecibirDamage(float damage)
+    {
+        vida -= damage;
+        Debug.Log("Vida enemigo: " + vida);
+        if (vida <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    //Colision con el jugador
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            JugadorController.Instance.RecibirDamage(damage);
+    }
+
+}
--- Juego 2d PJV2/Assets/Scripts/JugadorController.cs
+++ Juego 2d PJV2/Assets/Scripts/JugadorController.cs
@@ -2,12 +2,26 @@ using UnityEngine;
 
 public class JugadorController : MonoBehaviour
 {
+    public static JugadorController Instance;
     [Header("Configuracion Jugador")]
     [SerializeField] private float velocidad;
+    [SerializeField] private float vida;
     private float moverHorizontal;
     private float moverVertical;
     private Vector2 direccion;
     private Rigidbody2D miRigidbody2D;
+    // Singleton
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void OnEnable()
     {
@@ -24,5 +38,13 @@ public class JugadorController : MonoBehaviour
     void FixedUpdate()
     {
         miRigidbody2D.MovePosition(miRigidbody2D.position + direccion * (velocidad * Time.fixedDeltaTime));
+    }
+    // Daño al jugador
+    public void RecibirDamage(float damage)
+    {
+        vida -= damage;
+        if (vida <= 0)
+        {
+        }
     }
 }
