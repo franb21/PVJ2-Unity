e081205f7d6ba402564495efd22af3b193f1cfc0
franb21 <franbrg2@gmail.com>
2025-09-29 18:04:51 -0300

    Agrege un sistema de mejoras con button para prueba
    Agrege mejoras para la pistola con cooldown, damage, speed y cantidad.
    El SO configuras los limites y mejoras

--- Juego 2d PJV2/Assets/Scripts/Pistola.cs
+++ Juego 2d PJV2/Assets/Scripts/Pistola.cs
@@ -10,12 +10,28 @@ public class Pistola : MonoBehaviour
     private float cooldown;
     private float speed;
     private int cantidad;
-    private float tiempoDisparo = 0f;
+    
+    // Nivel de mejora acutal
+    private int damageLevel;
+    private int cooldownLevel;
+    private int speedLevel;
+    private int cantidadLevel;
 
+    // Text de valores de mejoras
+    private string damageText;
+    private string cooldownText;
+    private string speedText;
+    private string cantidadText;
+
+    private float tiempoDisparo = 0f;
     public float Damage { get => damage; }
     public float Cooldown { get => cooldown; }
     public float Speed { get => speed; }
     public int Cantidad { get => cantidad; }
+    public string DamageText { get => damageText; }
+    public string CooldownText { get => cooldownText; }
+    public string SpeedText { get => speedText; }
+    public string CantidadText { get => cantidadText; }
 
 
     private void Start()
@@ -24,6 +40,11 @@ public class Pistola : MonoBehaviour
         cooldown = pistolaData.Cooldown;
         speed = pistolaData.Speed;
         cantidad = pistolaData.Cantidad;
+
+        damageText = pistolaData.DamageText;
+        cooldownText = pistolaData.CooldownText;
+        speedText = pistolaData.SpeedText;
+        cantidadText = pistolaData.CantidadText;
     }
     void Update()
     {
@@ -40,5 +61,69 @@ public class Pistola : MonoBehaviour
                 Instantiate(pistolaData.ProyectilPrefab, transform.position + offset, transform.rotation);
             }
         }
+    }
+    public void MejorarCooldown()
+    {
+        cooldown -= pistolaData.CooldownMejora;
+        cooldownLevel++;
+        cooldownText = cooldown.ToString("0") + " / +" + pistolaData.CooldownMejora.ToString("0");
+
+        if (cooldownLevel >= pistolaData.MaxCooldown)
+        {
+            cooldownText = cooldown.ToString("0") + " ---Al maximo---";
+        }
+    }
+
+    public void MejorarDamage()
+    {
+        damage += pistolaData.DamageMejora;
+        damageLevel++;
+        damageText = damage.ToString("0") + " / +" + pistolaData.DamageMejora.ToString("0");
+        Debug.Log(" pistola mejorada" + damageText);
+        if (damageLevel >= pistolaData.MaxDamage)
+        {
+            damageText = damage.ToString("0") + " ---Al maximo---";
+        }
+    }
+
+    public void MejorarSpeed()
+    {
+        speed += pistolaData.SpeedMejora;
+        speedLevel++;
+        speedText = speed.ToString("0") + " / +" + pistolaData.SpeedMejora.ToString("0");
+
+        if (speedLevel >= pistolaData.MaxSpeed)
+        {
+            speedText = speed.ToString("0") + " ---Al maximo---";
+        }
+    }
+
+    public void MejorarCantidad()
+    {
+        cantidad += pistolaData.CantidadMejora;
+        cantidadLevel++;
+        cantidadText = cantidad.ToString("0") + " / +" + pistolaData.CantidadMejora.ToString("0");
+
+        if (cantidadLevel >= pistolaData.MaxCantidad)
+        {
+            cantidadText = cantidad.ToString("0") + " ---Al maximo---";
+        }
+    }
+
+    public bool IsMaxCooldown()
+    {
+        return cooldownLevel >= pistolaData.MaxCooldown;
+    }
+    public bool IsMaxDamage()
+    {
+        return damageLevel >= pistolaData.MaxDamage;
+    }
+    public bool IsMaxSpeed()
+    {
+        return speedLevel >= pistolaData.MaxSpeed;
+    }
+    public bool IsMaxCantidad()
+    {
+        return cantidadLevel >= pistolaData.MaxCantidad;
     }
 }
--- Juego 2d PJV2/Assets/Scripts/StatsScriptableObjet.cs
+++ Juego 2d PJV2/Assets/Scripts/StatsScriptableObjet.cs
@@ -13,9 +13,39 @@ public class StatsScriptableObjet : ScriptableObject
     [SerializeField] private float speed;
     [SerializeField] private int cantidad;
 
+    [Header("Incrementos por mejora")]
+    [SerializeField] private float cooldownMejora;
+    [SerializeField] private float damageMejora;
+    [SerializeField] private float speedMejora;
+    [SerializeField] private int cantidadMejora;
+
+    [Header("Limites de cantidad de mejoras")]
+    [SerializeField] private int maxCooldown;
+    [SerializeField] private int maxDamage;
+    [SerializeField] private int maxSpeed;
+    [SerializeField] private int maxCantidad;
+
+    [Header("Textos para mejora inicial")]
+    [SerializeField] private string cooldownText;
+    [SerializeField] private string damageText;
+    [SerializeField] private string speedText;
+    [SerializeField] private string cantidadText;
+
     public float Cooldown { get => cooldown; }
     public float Damage { get => damage; }
     public float Speed { get => speed; }
     public int Cantidad { get => cantidad; }
     public GameObject ProyectilPrefab { get => proyectilPrefab; }
+    public float CooldownMejora { get => cooldownMejora; }
+    public float DamageMejora { get => damageMejora; }
+    public float SpeedMejora { get => speedMejora; }
+    public int CantidadMejora { get => cantidadMejora; }
+    public int MaxCooldown { get => maxCooldown; }
+    public int MaxDamage { get => maxDamage; }
+    public int MaxSpeed { get => maxSpeed; }
+    public int MaxCantidad { get => maxCantidad; }
+    public string CooldownText { get => cooldownText; }
+    public string DamageText { get => damageText; }
+    public string SpeedText { get => speedText; }
+    public string CantidadText { get => cantidadText; }
 }
