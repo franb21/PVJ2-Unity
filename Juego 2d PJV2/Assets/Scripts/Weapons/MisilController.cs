5e60483c26b7efdb69e200625aa906ab7169133a
franb21 <franbrg2@gmail.com>
2025-10-19 07:24:25 -0300

    Nueva arma Misil
    Nueva arma que busca enemigos cuando se activa

---
+++ Juego 2d PJV2/Assets/Scripts/Weapons/MisilController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class MisilController : MonoBehaviour
+{
+    private Misil misil;
+    private Rigidbody2D rb;
+    private float vidaRestante;
+    [SerializeField] private float velocidad;
+    [SerializeField] private float tiempoVida;
+    private Vector2 direccion;
+
+    public void Inicializar(Misil m)
+    {
+        misil = m;
+    }
+
+    private void OnEnable()
+    {
+        // Evita errores
+        if (misil == null)
+        {
+            return;
+        }
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+            
+        vidaRestante = tiempoVida;
+
+        // Busca un enemigo y calcula la direccion hacia el
+        Transform objetivo = BuscarEnemigo();
+        if (objetivo != null)
+        {
+            direccion = ((Vector2)objetivo.position - rb.position).normalized;
+        }
+        else
+        {
+            direccion = JugadorController.Instance.UltimaDireccion.normalized;
+        }
+        rb.linearVelocity = direccion * velocidad;
+    }
+    private void Update()
+    {
+        vidaRestante -= Time.deltaTime;
+        if (vidaRestante <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    // Busca un enemigo aleatorio
+    private Transform BuscarEnemigo()
+    {
+        GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Enemigo");
+        if (enemigos.Length == 0)
+        {
+            return null;
+        }
+        int indice = Random.Range(0, enemigos.Length);
+
+        return enemigos[indice].transform;
+    }
+    // Detecta colision con enemigo y lo daña
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemigo"))
+        {
+            Enemigo enemigo = collision.GetComponent<Enemigo>();
+            if (enemigo != null)
+            {
+                enemigo.RecibirDamage(misil.Damage);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
